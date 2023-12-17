14067434c16f6d3db1c0701b251b2d3ddc344e14
Ahmed Hamada <ahmedhamada496@gmail.com>
2023-12-17 13:52:43 +0200

    update cros

--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,50 @@
-import express from "express"
-import bodyParser from "body-parser"
-import morgan from "morgan"
-import helmet from "helmet"
-import dotenv from "dotenv"
-import rateLimit from "express-rate-limit"
-import mongoSanitize from "express-mongo-sanitize"
-import cookieParser from "cookie-parser"
-import path from "path"
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import path from "path";
 
-import connectDB from "./utils/connectDB"
-import routes from "./routes"
-import logger from "./utils/logger"
-import cors from "cors"
-dotenv.config()
+import connectDB from "./utils/connectDB";
+import routes from "./routes";
+import logger from "./utils/logger";
+import cors from "cors";
+dotenv.config();
 
-const app = express()
+const app = express();
 //config.get<number>("PORT")
 // Get the PORT value from the environment variable
-const portString = process.env.PORT as string
+const portString = process.env.PORT as string;
 
 // Convert the PORT string to a number (if it's defined and valid)
-const PORT: number = parseInt(portString, 10) as number
+const PORT: number = parseInt(portString, 10) as number;
 
 if (PORT !== undefined && isNaN(PORT)) {
   // Handle the case where the PORT value is not a valid number
-  console.error("Invalid PORT value:", portString)
+  console.error("Invalid PORT value:", portString);
 }
 
 // Set trust proxy to true to trust proxy headers
-app.set("trust proxy", true)
+app.set("trust proxy", true);
 // Set security HTTP headers
-app.use(helmet())
+app.use(helmet());
 // Parse incoming request bodies
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser())
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 if (process.env.NODE_ENV?.trim() === "development") {
-  app.use(morgan("dev"))
+  app.use(morgan("dev"));
 } else {
   app.use(
     morgan(
-      ":date[web] :remote-addr :method :url :status :response-time ms - :res[content-length]"
-    )
-  )
+      ":date[web] :remote-addr :method :url :status :response-time ms - :res[content-length]",
+    ),
+  );
 }
 // set rate limiter for the ips to secure from Brute-force attack
 const limiter = rateLimit({
@@ -53,27 +53,23 @@ const limiter = rateLimit({
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: "too many requests from the same IP, Please try again in one hour",
-})
+});
 // Apply the rate limiting middleware to all requests
-app.use(limiter)
+app.use(limiter);
 
 // Data sanitization against NoSQL query injection
-app.use(mongoSanitize())
+app.use(mongoSanitize());
 
 // Data sanitization against XSS
 // app.use(sanitizeRequestData)// i comment it because it make problem with params it return it as object object
 
-app.set("trust proxy", false)
-app.use((req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "*")
-  res.set("Access-Control-Expose-Headers", "x-total-count")
-  res.set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS")
-  res.set("Access-Control-Allow-Headers", "Content-Type,authorization")
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200)
-  }
-  next()
-})
+app.set("trust proxy", false);
+app.use(
+  cors({
+    origin: [/http:\/\/localhost:\d*/, "https://cubuild.net"],
+    optionsSuccessStatus: 200,
+  }),
+);
 // disable core for the front local dev
 // app.use(
 //   cors({
@@ -84,11 +80,11 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res) => {
   res.send(
-    `<h1 style="text-align:center; padding-top:100px" >Up And Running🚀</h1>`
-  )
-})
+    `<h1 style="text-align:center; padding-top:100px" >Up And Running🚀</h1>`,
+  );
+});
 app.listen(PORT, async () => {
-  logger.info(`app is running on http://localhost:1337/`)
-  await connectDB()
-  routes(app)
-})
+  logger.info(`app is running on ${PORT}`);
+  await connectDB();
+  routes(app);
+});
--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -1,22 +1,22 @@
-import mongoose from "mongoose"
-import logger from "./logger"
-import dotenv from "dotenv"
-dotenv.config()
+import mongoose from "mongoose";
+import logger from "./logger";
+import dotenv from "dotenv";
+dotenv.config();
 // const mongodb_uri = config.get<string>("MONGO_DB_URI")
-const mongodb_uri = process.env.MONGO_DB_URI as string
-const mongodb_local_uri = process.env.MONGO_DB_LOCAL_URL as string
+const mongodb_uri = process.env.MONGO_DB_URI as string;
+const mongodb_local_uri = process.env.MONGO_DB_LOCAL_URL as string;
 
 async function connectDB() {
-  var runningDB = mongodb_uri
+  var runningDB = mongodb_uri;
   try {
     if (process.env.NODE_ENV?.trim() === "development") {
-      runningDB = mongodb_local_uri
+      runningDB = mongodb_local_uri;
     }
-    await mongoose.connect(runningDB)
-    logger.info("db is connected successfully!")
+    await mongoose.connect(runningDB);
+    logger.info("db is connected successfully!");
   } catch (error) {
-    logger.error(`some this wrong happened like ${error}`)
-    process.exit(1)
+    logger.error(`some this wrong happened like ${error}`);
+    process.exit(1);
   }
 }
-export default connectDB
+export default connectDB;
